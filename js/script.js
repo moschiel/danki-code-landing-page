b5825022c5aca94e05e9471812fc40d852995f8f
M@schie1 <moschiel@hotmail.com>
2020-10-11 21:09:12 -0300

    adicionado jquery, aplicado slideToogle ao menu-mobile

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menuIcon = $('img.menu');
+let menuMobile = $('.mobile-menu');
+
+//aplica evento de click para inverter visibilidade do menu mobile
+$(menuIcon).click(function(){
+  menuMobile.slideToggle();
+});
+
+//evento resize, desaparece o menu-mobile se tela maior que 760px 
+$(window).resize(function () { 
+  if($(window).width() > 760 && menuMobile.css('display') != 'none') {
+    menuMobile.css('display', 'none');
+  }
+});
